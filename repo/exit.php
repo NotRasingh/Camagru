5d3ad070051cc130c7d9c2cae56bc4a3825b7bec
Rahul SINGH <rasingh@e4r10p5.wethinkcode.co.za>
2018-11-29 14:28:39 +0200

    Neaten up

--- repo/exit.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Location: index.php');
-echo "<script>alert('You have successfully logged out.')</script>"
-?>
--- repo/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['src']))
-{
-    $_SESSION['url'] = $_POST['src'];
-}
-header('Location: info.php');
-?>
